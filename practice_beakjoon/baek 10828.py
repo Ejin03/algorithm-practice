b500dfa94f8371d1af8be43bb290eef29a5b8278
Ejin03 <103118816+Ejin03@users.noreply.github.com>
2022-07-26 15:39:37 +0900

    Update baek 10828.py

--- practice_beakjoon/baek 10828.py
+++ practice_beakjoon/baek 10828.py
@@ -1,29 +1,40 @@
+# 값을 받고 if 와 elif 또는 else로 마무리하며 처리하는 형식
+#값을 받고 만들어둔 형식들로 대조해가며 값을 처리함
+
 import sys
 
-list=[] #스택
+stack=[] 
+ 
+N = int(sys.stdin.readline()) #갯수 받기
 
-def push(a): #스택 넣기
-  list.append(a)
-
-def pop(): #스택 빼기
-  if list:
-    pop(list)
-  else:
-    print("-1")
-
-def size(): #스택 안 갯수
-  print(len(list))
-
-def empty(): #스택이 비어있다면 -1 있다면 1을
-  if not list:
-    print("1")
-  else:
-    print("-1")
-
-def top(): #스택이 비어있으면 -1을 아니면 가장 위에있는 값을
-  if not list:
-    print("-1")
-  else:
-    print(list[-1])
+for i in range(N):
+    a = sys.stdin.readline().split() #값받기
     
-N=int(sys.stdin.readline().rstrip())
+    order = a[0] #명령어 a[1]은 값
+    
+    if order == "push":
+    	stack.append(int(a[1]))
+
+    elif order == "pop":
+        if len(stack) == 0: #stack안이 비어있다면
+            print("-1")
+        else:
+            print(stack.pop())
+    
+    elif order == "size": 
+        if not len(stack) == 0:
+            print(len(stack))
+        else:
+            print("0")
+            
+    elif order == "empty":
+        if len(stack) == 0:
+            print("1")
+        else:
+            print("0")
+    
+    elif order == "top":
+        if len(stack) == 0:
+            print("-1")
+        else: 
+            print(stack[-1])
